4d1b10c989079bd106256776dc34d9baf0df060f
likai1216951219 <1216951219@qq.com>
2021-10-23 04:26:07 +0800

    完善了优惠券类以及其两个派生类

---
+++ Codes/Shop/coupon.cpp
@@ -0,0 +1,32 @@
+#include "../Shop/coupon.h"
+
+void Coupon::checkOverdue() {
+    time_t now = time(0);
+    if (now > getOutDate()) setCouponState(overdue);
+    cout<<"很遗憾，您本张优惠券过期了"<<'\n';
+}
+
+
+double DiscountCoupon::saleMethod(double x) {
+    return (x * discount);
+}
+
+void DiscountCoupon::changeStateToUsed() {
+    setCouponState(used);
+    cout << "您成功使用了本张折扣优惠券" << '\n';
+    cout << "本优惠券打的折扣是 " << getDiscount() << "折" << '\n';
+    cout << "期待您的下次使用" << '\n';
+}
+
+
+double FullReduceCoupon::saleMethod(double x) {
+    double result = (x < standard) ? x : x - reduction;
+    return result;
+}
+
+void FullReduceCoupon::changeStateToUsed() {
+    setCouponState(used);
+    cout << "您成功使用了本张满减优惠券" << '\n';
+    cout << "本优惠券为满" << getStandard() << "减" <<getReduction()<<"！\n";
+    cout << "期待您的下次使用" << '\n';
+}
--- Codes/Shop/coupon.h
+++ Codes/Shop/coupon.h
@@ -1,13 +1,86 @@
+#pragma once
 
+#include<iostream>
+#include<string>
+#include<vector>
+#include<ctime>
 
-class Coupon{
+using namespace std;
+
+enum CouponState {
+    unused, used, overdue
+};//未使用、已使用、过期的优惠券
+
+//预声明
+class Customer;
+
+//预声明
+class Shop;
+
+class Coupon {
+public:
+    //构造函数默认优惠券使用时间是一个月 2592000天
+    Coupon(Customer *c, Shop *s) : customer(c), shop(s), couponState(unused) {
+        time_t lt = time(nullptr);
+        outDate = lt + 2592000;
+    }
+
+    //使用折扣以后的售价，参数x为原价，返回折后价格
+    virtual double saleMethod(double x) { return 0; }
+
+    virtual void changeStateToUsed() { couponState = used; }
+
+    void checkOverdue();
+
+    Customer *getCustomer() { return customer; }
+
+    Shop *getShop() { return shop; }
+
+    CouponState getCouponState() { return couponState; }
+
+    CouponState setCouponState(CouponState cs) { couponState = cs; }
+
+    time_t getOutDate() const { return outDate; }
+
+private:
+    Customer *customer;
+    Shop *shop;
+    CouponState couponState;
+    time_t outDate;
 
 };
 
-class DiscountCoupon:public Coupon{
+//折扣类型的优惠
+class DiscountCoupon : public Coupon {
+public:
+    DiscountCoupon(Customer *c, Shop *s, double ds) : Coupon(c, s), discount(ds) {}
 
+    //返回使用折扣类型使商品打折后的价格
+    double saleMethod(double x) override;
+
+    void changeStateToUsed() override;
+
+    double getDiscount() const { return discount; }
+
+private:
+    double discount;
 };
 
-class FullReduceCoupon:public Coupon{
+//满减类型的优惠
+class FullReduceCoupon : public Coupon {
+public:
+    FullReduceCoupon(Customer *c, Shop *s, double std, double rt) : Coupon(c, s), standard(std), reduction(rt) {}
 
+    //返回使用满减类型使商品减价后的价格
+    double saleMethod(double x) override;
+
+    void changeStateToUsed() override;
+
+    double getStandard() const { return standard; }
+
+    double getReduction() const { return reduction; }
+
+private:
+    double standard;
+    double reduction;
 };
