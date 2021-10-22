0d54f0febdd2ca614737f62492245cbcbcb17737
likai1216951219 <1216951219@qq.com>
2021-10-23 05:43:17 +0800

    主会场和分会场的初步设计

---
+++ Codes/Venue/venue.cpp
@@ -0,0 +1,42 @@
+#include "venue.h"
+
+void MainVenue::showBasicInformation() {
+
+    cout << "本次天猫购物节主会场有以下分会场：" << '\n';
+}
+
+void MainVenue::findShops(string name) {
+
+}
+
+void ClothingVenue::showBasicInformation() {
+
+}
+
+void ClothingVenue::initialize() {
+
+}
+
+void SnacksVenue::showBasicInformation() {
+
+}
+
+void SnacksVenue::initialize() {
+
+}
+
+void ElectronicVenue::showBasicInformation() {
+
+}
+
+void ElectronicVenue::initialize() {
+
+}
+
+void FruitsVenue::showBasicInformation() {
+
+}
+
+void FruitsVenue::initialize() {
+
+}
