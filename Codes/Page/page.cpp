51dad39ec1d1c5bb36f237357ed1d50a856d58ee
Richy_D <1245413737@qq.com>
2021-10-29 15:45:07 +0800

    完善了组合模式
    testPage函数可进行一个页面访问测试，可以进入页面、返回上一级和执行简单命令

--- Codes/Page/page.cpp
+++ Codes/Page/page.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 //命令构造函数
@@ -11,32 +12,69 @@ Command::Command() {
 
 //页面构造函数
 Page::Page(const char* p, const char* d) {
-    prePage = NULL;
-    prompt.assign(p);
-    descrip.assign(d);
-}
-
-//页面构造函数
-Page::Page(Page* pre, const char* p, const char* d) {
-    prePage = pre;
+    end = false;
     prompt.assign(p);
     descrip.assign(d);
 }
 
 //进入页面
 void Page::execute() {
-    //显示页面
-    show();
+    string cmd;
+    //等待用户输入命令
+    while (!end) {
+        //清屏
+        system("cls");
+        //显示页面
+        show();
+
+        cout << "请输入命令:" << endl;
+        cin >> cmd;
+        //找到匹配命令执行
+        for (int i = 0; i < cmds.size(); i++) {
+            if (cmds[i]->prompt == cmd) {
+                cmds[i]->execute();
+                if (cmds[i]->prompt == string("back")) {
+                    end = true;
+                }
+            }
+        }
+    }
 }
 
 //显示页面内容
 void Page::show() {
     //打印命令列表等内容
-    cout << "这里是{" << descrip <<"}页面" << endl;
+    cout << "这里是{" << descrip << "}页面" << endl;
+
+    //打印命令列表
+    cout << "命令列表" << endl;
+    for (int i = 0; i < cmds.size(); i++) {
+        cout << i + 1 << "." << cmds[i]->prompt << " : " << cmds[i]->descrip << endl;
+    }
 }
 
-//一个测试函数，创建一个主页面实例并执行
-void testPage() {    
-    Page Home("home", "主会场");
-    Home.execute();
+//这是一个测试函数，可以放到main.cpp中进行测试
+void testPage() {
+    //定义页面类
+    Page* home = new Page("home", "主会场");
+    Page* search = new Page("search", "搜索商品");
+    //定义简单命令
+    AddGoods addRcvr;
+    Command* addCmd = new SimpleCommand<AddGoods>(&addRcvr, &AddGoods::Action);
+    Back backRcvr;
+    Command* backCmd = new SimpleCommand<Back>(&backRcvr, &Back::Action);
+
+    //初始化命令
+    addCmd->prompt = "addGoods";
+    addCmd->descrip = "将商品加入购物车";
+    backCmd->prompt = "back";
+    backCmd->descrip = "返回上一级页面";
+    //初始化页面的命令列表
+    home->cmds.push_back(search);
+    home->cmds.push_back(addCmd);
+    search->cmds.push_back(addCmd);
+    search->cmds.push_back(backCmd);
+
+    //进入主页面
+    home->execute();
 }
--- Codes/Page/page.h
+++ Codes/Page/page.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std;
 
 //命令类，所有用户使用的指令都继承于命令类
@@ -18,15 +19,52 @@ protected:
     Command();
 };
 
+//简单命令类，继承于Command类的模板类，即简单的命令“个别物”
+template <class Receiver>
+class SimpleCommand :public Command {
+public:
+    typedef void (Receiver::* Action)();
+
+    SimpleCommand(Receiver* r, Action a) :
+        _receiver(r), _action(a) { }
+
+    virtual void execute();
+private:
+    Action _action;
+    Receiver* _receiver;
+};
+
+//简单命令模板类的执行函数方法
+template <class Receiver>
+void SimpleCommand<Receiver>::execute() {
+    (_receiver->*_action)();
+}
+
+//定义具体的请求类
+class AddGoods {
+public:
+    void Action() {
+        cout << "将商品加入购物车" << endl;
+    }
+};
+
+//定义具体的请求类
+class Back {
+public:
+    void Action() {
+        cout << "返回上一级页面" << endl;
+    }
+};
+
 //页面类，继承于命令类，因为用户可以通过指令完成“进入页面”这一动作，页面也是一个命令
 //用户在一个页面中可以使用多个不同的指令,即页面是命令的一个“复合物”
 class Page :public Command {
 public:
+    //本页面的命令列表
+    vector<Command* > cmds;
+
     //没有上一页面时的构造函数
     Page(const char*, const char*);
-
-    //有上一页面时的构造函数
-    Page(Page*, const char*, const char*);
 
     //显示页面内容
     void show();  
@@ -34,9 +72,6 @@ public:
     //命令的执行过程函数
     virtual void execute();
 private:
-    //指向上一个页面的指针
-    Page* prePage;  
-
-    //本页面的命令列表
-    vector<Command* > cmds; 
+    //页面是否结束
+    bool end;
 };
