95e9b56e6c8a00ff3363ed552566c677ea135b53
jiangyong <1729145790@qq.com>
2021-10-30 19:43:02 +0800

    Revert "抽象工厂模式测试"
    This reverts commit bdeb889ff1385aaa7f83a213775cdb540826974e.

--- Codes/main.cpp
+++ Codes/main.cpp
@@ -10,7 +10,6 @@
 #include"PersonalInformation/SearchEngine.h"
 #include"Venue/venue.h"
 #include"Order/balance.h"
-#include"Shop/clothingAbstractFactory.h"
 #include<iostream>
 #include<ctime>
 #include<map>
@@ -40,33 +39,33 @@ int main() {
 //用于测试单例模式的接口
 //    testSingleton();
 
-    // Customer c("cnm", "134560", "14", male, 1222.4);
+    Customer c("cnm", "134560", "14", male, 1222.4);
 
-    // Shop fruit_1("hhhhhhh", "Happy");
-    // fruit_1.addGoods(Goods("apple", 5, &fruit_1), 10);
-    // fruit_1.addGoods(Goods("banana", 25, &fruit_1), 10);
+    Shop fruit_1("hhhhhhh", "Happy");
+    fruit_1.addGoods(Goods("apple", 5, &fruit_1), 10);
+    fruit_1.addGoods(Goods("banana", 25, &fruit_1), 10);
 
-    // Shop fruit_2("ssssss", "Sad");
-    // fruit_2.addGoods(Goods("pear", 10, &fruit_2), 10);
-    // fruit_2.addGoods(Goods("orange", 30, &fruit_2), 10);
+    Shop fruit_2("ssssss", "Sad");
+    fruit_2.addGoods(Goods("pear", 10, &fruit_2), 10);
+    fruit_2.addGoods(Goods("orange", 30, &fruit_2), 10);
 
-    // Shop fruit_3("bbbbbb", "Boring");
-    // fruit_3.addGoods(Goods("mango", 15, &fruit_3), 10);
-    // fruit_3.addGoods(Goods("grape", 35, &fruit_3), 10);
-
-
-    // FruitsVenue& fruitInstance = FruitsVenue::getInstance();
-    // fruitInstance.addShop(fruit_1);
-    // fruitInstance.showBasicInformation();
+    Shop fruit_3("bbbbbb", "Boring");
+    fruit_3.addGoods(Goods("mango", 15, &fruit_3), 10);
+    fruit_3.addGoods(Goods("grape", 35, &fruit_3), 10);
 
 
-    // FruitsVenue& fruitInstance1 = FruitsVenue::getInstance();
-    // fruitInstance1.addShop(fruit_3);
-    // fruitInstance1.showBasicInformation();
+    FruitsVenue& fruitInstance = FruitsVenue::getInstance();
+    fruitInstance.addShop(fruit_1);
+    fruitInstance.showBasicInformation();
 
-    // MainVenue venue;
-    // SearchEngine s;
-    // s.searchGoods();
+
+    FruitsVenue& fruitInstance1 = FruitsVenue::getInstance();
+    fruitInstance1.addShop(fruit_3);
+    fruitInstance1.showBasicInformation();
+
+    MainVenue venue;
+    SearchEngine s;
+    s.searchGoods();
 }
 
 void testDecorator() {//测试装饰器模式
@@ -94,36 +93,4 @@ void testDecorator() {//测试装饰器模式
 
 void testSingleton() {//测试单例模式的接口
     ClothingVenue &instance = ClothingVenue::getInstance();
-}
-
-//抽象工厂方法的测试接口，用于为服装商店添加商品
-void addClothingShop(){//coded by jy
-    //获取服装会差单例
-    ClothingVenue& clothingVenue=ClothingVenue::getInstance();
-    //新建安踏服装店
-    Shop antaShop("安踏服装店","安踏");
-    //利用安踏工厂生产安踏商品
-    ClothingGoodsFactory* antaFactory=new AntaFactory();
-    Coats antaCoats=antaFactory->getCoats("安踏外套",500,&antaShop);
-    Pants antaPants=antaFactory->getPants("安踏休闲裤",200,&antaShop);
-    //加入商店
-    antaShop.addGoods(antaCoats,10);
-    antaShop.addGoods(antaPants,20);
-
-    
-    //新建阿迪服装店
-    Shop adidasShop("阿迪达斯服装店","阿迪达斯");
-    //利用阿迪工厂生产安踏商品
-    ClothingGoodsFactory* adidasFactory=new AdidasFactory();
-    Coats adidasCoats=adidasFactory->getCoats("阿迪外套",700,&antaShop);
-    Pants adidasPants=adidasFactory->getPants("阿迪休闲裤",300,&antaShop);
-    //加入商店
-    adidasShop.addGoods(antaCoats,5);
-    adidasShop.addGoods(antaPants,10);
-
-    //test output
-    antaShop.showInformation();
-    adidasShop.showInformation();
-    antaCoats.showInfo();
-    adidasCoats.showInfo();
 }
