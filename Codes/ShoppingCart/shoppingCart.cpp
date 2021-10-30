745318e809a6f1f2daa47247c8d6cae546f3d39d
Richy_D <1245413737@qq.com>
2021-10-30 23:27:32 +0800

    实现数据访问对象模式
    给购物车加了一个数据实体类CartData，购物车的数据实际都存在这个类，原来的ShoppingCart类变成了一个访问接口类

--- Codes/ShoppingCart/shoppingCart.cpp
+++ Codes/ShoppingCart/shoppingCart.cpp
@@ -2,36 +2,42 @@
 #include "../PersonalInformation/customer.h"
 #include"../Order/order.h"
 
-void ShoppingCart::addGoods(Goods gd){
+//添加商品数据
+bool CartData::add(Goods gd) {
     if (goods.count(gd)){ //判断是否已有该商品
         goods[gd]++;
-    }
-    else{
-        goods.insert(map<Goods, int>::value_type(gd, 1));
-
-        //将购物车绑定为商品的Observer
-        gd.attach(this);
-    }
-}
-
-bool ShoppingCart::removeGoods(Goods gd){
-    if (goods.count(gd)){
-        goods[gd]--;
-        if (goods[gd] == 0){
-            goods.erase(gd);
-            
-            //将购物车从商品的Observer列表中删除
-            gd.detach(this);
-        }
         return true;
     }
     else{
-        cout << "error! 购物车中不存在该商品\n";
+        goods.insert(map<Goods, int>::value_type(gd, 1));
         return false;
     }
 }
 
-bool ShoppingCart::getGoods(string name,Goods& gd){
+//删除商品数据
+bool CartData::remove(Goods gd) {
+    if (goods.count(gd)){
+        goods.erase(gd);     
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+//设置商品数量
+bool CartData::setNum(Goods gd, int n) {
+    if (goods.count(gd)){
+        goods[gd] = n;
+        return true;
+    }
+    else{
+        return false;
+    }    
+}
+
+//获取商品数据
+bool CartData::get(string name,Goods& gd) {
     map<Goods,int>::iterator it=goods.begin();
     for(it=goods.begin();it!=goods.end();it++){
         if(it->first.getName()==name){
@@ -39,8 +45,54 @@ bool ShoppingCart::getGoods(string name,Goods& gd){
             return true;
         }
     }
-    cout<<"error! 不存在该商品\n";
     return false;
+}
+
+map<Goods,int>& CartData::getMap() {
+    return goods;
+}
+
+void ShoppingCart::addGoods(Goods gd){
+    if(!goodsData->add(gd)) {
+        //将购物车绑定为商品的Observer
+        gd.attach(this);
+    }        
+}
+
+bool ShoppingCart::removeGoods(Goods gd){
+    if(goodsData->remove(gd)) {        
+        //将购物车从商品的Observer列表中删除
+        gd.detach(this);
+        return true;
+    }
+    else {        
+        cout << "error! 购物车中不存在该商品\n";
+        return false;
+    }
+}
+
+bool ShoppingCart::getGoods(string name,Goods& gd){
+    if(!goodsData->get(name,gd)) {
+        cout<<"error! 不存在该商品\n";
+        return false;
+    }
+    return true;
+}
+
+bool ShoppingCart::setGoodsNum(Goods gd, int n){
+    if(n < 1) {
+        cout<<"error! 不设置的商品数量不能小于1\n";
+        return false;
+    }
+    if(!goodsData->setNum(gd, n)) {
+        cout<<"error! 不存在该商品\n";
+        return false;
+    }
+    return true;
+}
+
+map<Goods,int>& ShoppingCart::getGoodsMap() {
+    return goodsData->getMap();
 }
 
 bool ShoppingCart::generateOrder(map<Goods,int> gds){
@@ -58,10 +110,7 @@ bool ShoppingCart::generateOrder(map<Goods,int> gds){
         return false;
     }
     for(it=gds.begin();it!=gds.end();it++){//购物车中减去对应商品
-        goods[it->first]-=it->second;
-        if(goods[it->first]==0){
-            goods.erase(it->first);
-        }
+        goodsData->remove(it->first);
     }
     for(it=gds.begin();it!=gds.end();it++){//从店铺中减去对应商品
            
--- Codes/ShoppingCart/shoppingCart.h
+++ Codes/ShoppingCart/shoppingCart.h
@@ -7,6 +7,24 @@ using namespace std;
 //预声明
 class Customer;
 
+//购物车数据实体
+//数据访问对象模式
+class CartData {
+public:
+    //添加商品数据
+    bool add(Goods);
+    //删除商品数据
+    bool remove(Goods);
+    //获取商品数据
+    bool get(string, Goods&);
+    //获取商品map
+    map<Goods,int>& getMap();
+    //设置商品数量
+    bool setNum(Goods, int);
+private:
+    map<Goods,int> goods;
+};
+
 class ShoppingCart{
 public:
     ShoppingCart(Customer* c){
@@ -15,16 +33,17 @@ public:
     void addGoods(Goods gd);
     bool removeGoods(Goods gd);
     bool getGoods(string name,Goods& gd);
+    bool setGoodsNum(Goods, int);
 
     //输入为一个Goods的数组指针
     //若商品售罄则返回false
     bool generateOrder(map<Goods,int> gds);
-    map<Goods,int>& getGoodsMap(){return goods;}
+    map<Goods,int>& getGoodsMap();
 
     //Observer同步更新
     void update(Goods*);
 
 private:
-    map<Goods,int> goods;//为一个map的key-value映射表，初始为空
+    CartData* goodsData;//为一个map的key-value映射表，初始为空
     Customer *customer;
 };
