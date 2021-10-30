4f76f9d424cf380f5cc8b5ab6b6f0abc38cb5082
jiangyong <1729145790@qq.com>
2021-10-30 19:55:43 +0800

    回滚了一下版本

--- Codes/main.cpp
+++ Codes/main.cpp
@@ -40,33 +40,33 @@ int main() {
 //用于测试单例模式的接口
 //    testSingleton();
 
-    // Customer c("cnm", "134560", "14", male, 1222.4);
+    Customer c("cnm", "134560", "14", male, 1222.4);
 
-    // Shop fruit_1("hhhhhhh", "Happy");
-    // fruit_1.addGoods(Goods("apple", 5, &fruit_1), 10);
-    // fruit_1.addGoods(Goods("banana", 25, &fruit_1), 10);
+    Shop fruit_1("hhhhhhh", "Happy");
+    fruit_1.addGoods(Goods("apple", 5, &fruit_1), 10);
+    fruit_1.addGoods(Goods("banana", 25, &fruit_1), 10);
 
-    // Shop fruit_2("ssssss", "Sad");
-    // fruit_2.addGoods(Goods("pear", 10, &fruit_2), 10);
-    // fruit_2.addGoods(Goods("orange", 30, &fruit_2), 10);
+    Shop fruit_2("ssssss", "Sad");
+    fruit_2.addGoods(Goods("pear", 10, &fruit_2), 10);
+    fruit_2.addGoods(Goods("orange", 30, &fruit_2), 10);
 
-    // Shop fruit_3("bbbbbb", "Boring");
-    // fruit_3.addGoods(Goods("mango", 15, &fruit_3), 10);
-    // fruit_3.addGoods(Goods("grape", 35, &fruit_3), 10);
-
-
-    // FruitsVenue& fruitInstance = FruitsVenue::getInstance();
-    // fruitInstance.addShop(fruit_1);
-    // fruitInstance.showBasicInformation();
+    Shop fruit_3("bbbbbb", "Boring");
+    fruit_3.addGoods(Goods("mango", 15, &fruit_3), 10);
+    fruit_3.addGoods(Goods("grape", 35, &fruit_3), 10);
 
 
-    // FruitsVenue& fruitInstance1 = FruitsVenue::getInstance();
-    // fruitInstance1.addShop(fruit_3);
-    // fruitInstance1.showBasicInformation();
+    FruitsVenue& fruitInstance = FruitsVenue::getInstance();
+    fruitInstance.addShop(fruit_1);
+    fruitInstance.showBasicInformation();
 
-    // MainVenue venue;
-    // SearchEngine s;
-    // s.searchGoods();
+
+    FruitsVenue& fruitInstance1 = FruitsVenue::getInstance();
+    fruitInstance1.addShop(fruit_3);
+    fruitInstance1.showBasicInformation();
+
+    MainVenue venue;
+    SearchEngine s;
+    s.searchGoods();
 }
 
 void testDecorator() {//测试装饰器模式
