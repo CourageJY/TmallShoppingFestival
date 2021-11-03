a8b223bcfe433b84d7b4ed3b399f4a2d36691b80
jiangyong <1729145790@qq.com>
2021-11-03 23:32:17 +0800

    合并了冲突问题

--- Codes/Venue/venue.cpp
+++ Codes/Venue/venue.cpp
@@ -1,29 +1,12 @@
 #include "venue.h"
-<<<<<<< Updated upstream
-=======
-#include "../tools.h"//需在.cpp中包含tools.h
-#include "../PersonalInformation/customer.h"
-
-void MainVenue::findShops(string name) {
->>>>>>> Stashed changes
 
 #include "../PersonalInformation/customer.h"
 #include "../tools.h"  //需在.cpp中包含tools.h
 
 //设置分会场中指定店铺的所有商品属性
-<<<<<<< Updated upstream
 void MainVenue::setGoods(string name, vector<string> names,
                          vector<double> prices, vector<int> months) {
     for (auto&& i : shops) {
-=======
-void ClothingVenue::setGoods(string name, vector<string> names, vector<double> prices, vector<int> months) {
-    // for (vector<Shop*>::iterator iter = shops.begin(); iter != shops.end(); iter++)
-    //     if ((*iter)->getName() == name) {
-    //         iter->setGoods(names, prices, months);
-    //         return;
-    //     }
-    for (auto &&i: shops) {
->>>>>>> Stashed changes
         if (i->getName() == name) {
             i->setGoods(names, prices, months);
             return;
@@ -32,105 +15,16 @@ void ClothingVenue::setGoods(string name, vector<string> names, vector<double> p
 }
 
 //返回分会场的所有商品
-<<<<<<< Updated upstream
 map<Goods, int> MainVenue::getGoods() {
     map<Goods, int> goods;
     for (auto&& i : shops) {
         map<Goods, int> temp = i->getGoods();
-=======
-map<Goods, int> ClothingVenue::getGoods() {
-    map<Goods, int> goods;
-    for (auto &&iter: shops) {
-        map<Goods, int> temp = iter->getGoods();
         goods.insert(temp.begin(), temp.end());
     }
     return goods;
 }
 
 //返回分会场指定店铺的所有商品
-map<Goods, int> ClothingVenue::getGoods(string name) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name)
-            return iter->getGoods();
-    return map<Goods, int>();
-}
-
-//设置分会场中指定店铺的所有商品属性
-void SnacksVenue::setGoods(string name, vector<string> names, vector<double> prices, vector<int> months) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name) {
-            iter->setGoods(names, prices, months);
-            return;
-        }
-}
-
-//返回分会场的所有商品
-map<Goods, int> SnacksVenue::getGoods() {
-    map<Goods, int> goods;
-    for (auto &&iter: shops) {
-        map<Goods, int> temp = iter->getGoods();
-        goods.insert(temp.begin(), temp.end());
-    }
-    return goods;
-}
-
-//返回分会场指定店铺的所有商品
-map<Goods, int> SnacksVenue::getGoods(string name) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name)
-            return iter->getGoods();
-    return map<Goods, int>();
-}
-
-//设置分会场中指定店铺的所有商品属性
-void ElectronicVenue::setGoods(string name, vector<string> names, vector<double> prices, vector<int> months) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name) {
-            iter->setGoods(names, prices, months);
-            return;
-        }
-}
-
-//返回分会场的所有商品
-map<Goods, int> ElectronicVenue::getGoods() {
-    map<Goods, int> goods;
-    for (auto &&iter: shops) {
-        map<Goods, int> temp = iter->getGoods();
-        goods.insert(temp.begin(), temp.end());
-    }
-    return goods;
-}
-
-//返回分会场指定店铺的所有商品
-map<Goods, int> ElectronicVenue::getGoods(string name) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name)
-            return iter->getGoods();
-    return map<Goods, int>();
-}
-
-//设置分会场中指定店铺的所有商品属性
-void FruitsVenue::setGoods(string name, vector<string> names, vector<double> prices, vector<int> months) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name) {
-            iter->setGoods(names, prices, months);
-            return;
-        }
-}
-
-//返回分会场的所有商品
-map<Goods, int> FruitsVenue::getGoods() {
-    map<Goods, int> goods;
-    for (auto &&iter: shops) {
-        map<Goods, int> temp = iter->getGoods();
->>>>>>> Stashed changes
-        goods.insert(temp.begin(), temp.end());
-    }
-    return goods;
-}
-
-//返回分会场指定店铺的所有商品
-<<<<<<< Updated upstream
 map<Goods, int> MainVenue::getGoods(string name) {
     for (auto&& i : shops)
         if (i->getName() == name) return i->getGoods();
@@ -138,17 +32,6 @@ map<Goods, int> MainVenue::getGoods(string name) {
 }
 
 void MainVenue::showBasicInformation(Customer* customer) {
-=======
-map<Goods, int> FruitsVenue::getGoods(string name) {
-    for (auto &&iter: shops)
-        if (iter->getName() == name)
-            return iter->getGoods();
-    return map<Goods, int>();
-}
-
-
-void MainVenue::showBasicInformation(Customer *customer) {
->>>>>>> Stashed changes
     cout << "本次天猫购物节主会场有以下分会场：" << '\n';
     cout << "1. 服装会场" << '\n';
     cout << "2. 零食会场" << '\n';
@@ -156,19 +39,10 @@ void MainVenue::showBasicInformation(Customer *customer) {
     cout << "4. 水果会场" << '\n';
 }
 
-<<<<<<< Updated upstream
-void MainVenue::showInformation(Customer* customer) {
-    string info(
-        "请选择你想去的会场(1-4, "
-        "0退出，5搜索商品，6进入购物车，7查看个人信息)：");
-    while (1) {
-        system("cls");  //清空屏幕
-=======
 void MainVenue::showInformation(Customer *customer, ProxyPatternCustomer *proxyPatternCustomer) {
     string info("请选择你想去的会场(1-4, 0退出，5搜索商品，6进入购物车，7查看个人信息)：");
     while (1) {
         system("cls");//清空屏幕
->>>>>>> Stashed changes
         cout << "本次天猫购物节主会场有以下分会场：" << '\n' << endl;
         cout << "1. 服装会场" << '\n';
         cout << "2. 零食会场" << '\n';
@@ -198,16 +72,12 @@ void MainVenue::showInformation(Customer *customer, ProxyPatternCustomer *proxyP
                 customer->showShoppingCart();
                 continue;
             case 7:
-<<<<<<< Updated upstream
-                customer->viewBasicInformation();
-=======
                 proxyPatternCustomer->giveRealCustomer(customer);
                 // The profile will be loaded from disk
                 proxyPatternCustomer->viewBasicInformation();
                 cout << "\n";
                 cout << "The profile has initialized ,does not need to be loaded from disk" << endl;
                 proxyPatternCustomer->viewBasicInformation();
->>>>>>> Stashed changes
                 continue;
         }
     }
@@ -246,21 +116,13 @@ void MainVenue::listfind(string name, int t) {
 }
 
 //显示分会场的所有商品
-<<<<<<< Updated upstream
 void ClothingVenue::showBasicInformation(Customer* customer) {
-=======
-void ClothingVenue::showBasicInformation(Customer *customer) {
->>>>>>> Stashed changes
     int count = this->shops.size();
     while (1) {
         system("cls");
         cout << "服装会场有以下店铺：" << '\n';
         int k = 1;
-<<<<<<< Updated upstream
         for (auto&& i : this->shops) {
-=======
-        for (auto &&i: this->shops) {
->>>>>>> Stashed changes
             cout << '\n' << k;
             k++;
             cout << "\n-----------------\n";
@@ -279,32 +141,20 @@ void ClothingVenue::showBasicInformation(Customer *customer) {
             return;
         }
         if (no <= count) {
-<<<<<<< Updated upstream
             vector<Shop*> shops = getShops();
-=======
-            vector<Shop *> shops = getShops();
->>>>>>> Stashed changes
             shops[no - 1]->showInformation(customer);
             continue;
         }
     }
 }
 
-<<<<<<< Updated upstream
 void SnacksVenue::showBasicInformation(Customer* customer) {
-=======
-void SnacksVenue::showBasicInformation(Customer *customer) {
->>>>>>> Stashed changes
     int count = this->shops.size();
     while (1) {
         system("cls");
         cout << "零食会场有以下店铺：" << '\n';
         int k = 1;
-<<<<<<< Updated upstream
         for (auto&& i : this->shops) {
-=======
-        for (auto &&i: this->shops) {
->>>>>>> Stashed changes
             cout << '\n' << k;
             k++;
             cout << "\n-----------------\n";
@@ -323,32 +173,20 @@ void SnacksVenue::showBasicInformation(Customer *customer) {
             return;
         }
         if (no <= count) {
-<<<<<<< Updated upstream
             vector<Shop*> shops = getShops();
-=======
-            vector<Shop *> shops = getShops();
->>>>>>> Stashed changes
             shops[no - 1]->showInformation(customer);
             continue;
         }
     }
 }
 
-<<<<<<< Updated upstream
 void ElectronicVenue::showBasicInformation(Customer* customer) {
-=======
-void ElectronicVenue::showBasicInformation(Customer *customer) {
->>>>>>> Stashed changes
     int count = this->shops.size();
     while (1) {
         system("cls");
         cout << "电器会场有以下店铺：" << '\n';
         int k = 1;
-<<<<<<< Updated upstream
         for (auto&& i : this->shops) {
-=======
-        for (auto &&i: this->shops) {
->>>>>>> Stashed changes
             cout << '\n' << k;
             k++;
             cout << "\n-----------------\n";
@@ -367,32 +205,20 @@ void ElectronicVenue::showBasicInformation(Customer *customer) {
             return;
         }
         if (no <= count) {
-<<<<<<< Updated upstream
             vector<Shop*> shops = getShops();
-=======
-            vector<Shop *> shops = getShops();
->>>>>>> Stashed changes
             shops[no - 1]->showInformation(customer);
             continue;
         }
     }
 }
 
-<<<<<<< Updated upstream
 void FruitsVenue::showBasicInformation(Customer* customer) {
-=======
-void FruitsVenue::showBasicInformation(Customer *customer) {
->>>>>>> Stashed changes
     int count = this->shops.size();
     while (1) {
         system("cls");
         cout << "水果会场有以下店铺：" << '\n';
         int k = 1;
-<<<<<<< Updated upstream
         for (auto&& i : this->shops) {
-=======
-        for (auto &&i: this->shops) {
->>>>>>> Stashed changes
             cout << '\n' << k;
             k++;
             cout << "\n-----------------\n";
@@ -403,7 +229,6 @@ void FruitsVenue::showBasicInformation(Customer *customer) {
         string info;
         info = "请选择你要去的商店:(1-";
         info += to_string(count);
-<<<<<<< Updated upstream
         info += "，0返回，*进行店铺搜索)";
         string ss;
         while (ss == "") cin >> ss;
@@ -411,9 +236,6 @@ void FruitsVenue::showBasicInformation(Customer *customer) {
             cout << "搜索店铺" << endl;
             // 这里还没写完
         }
-=======
-        info += "，0返回)";
->>>>>>> Stashed changes
         int no = getNum(info, count);
         if (no == 0) {
             // MainVenue mainvenue;
@@ -421,11 +243,7 @@ void FruitsVenue::showBasicInformation(Customer *customer) {
             return;
         }
         if (no <= count) {
-<<<<<<< Updated upstream
             vector<Shop*> shops = getShops();
-=======
-            vector<Shop *> shops = getShops();
->>>>>>> Stashed changes
             shops[no - 1]->showInformation(customer);
             continue;
         }
